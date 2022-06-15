90137b503c88723058a82c22da86b75bf111cc81
karimi <zmanyamyd5@gmail.com>
2022-06-15 15:27:49 +0430

    create LoginEmail Activiyt

---
+++ app/src/main/java/com/karimi/pms/activity/LoginEmail.kt
@@ -0,0 +1,12 @@
+package com.karimi.pms.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.karimi.pms.R
+
+class LoginEmail : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_email)
+    }
+}
