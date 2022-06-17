48ee013cfe15698c175a70cdb792af2747fbd18d
karimi <zmanyamyd5@gmail.com>
2022-06-17 19:44:20 +0430

    add pictures for login

--- app/src/main/java/com/karimi/pms/activity/Login.kt
+++ app/src/main/java/com/karimi/pms/activity/Login.kt
@@ -3,11 +3,25 @@ package com.karimi.pms.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.view.View
+import android.widget.Toast
 import com.karimi.pms.R
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.login_email.view.*
 
-class LoginEmail : AppCompatActivity() {
+class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
+
+        email_ac.continueEmail.setOnClickListener { toast("btn clicked") }
     }
+
+
+
+    private fun toast(text : String){
+        Toast.makeText(applicationContext,text, Toast.LENGTH_SHORT).show()
+    }
+
+
 }
