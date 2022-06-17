7a95a84a5f14fab649e1fd0972b43d12af63b626
karimi <zmanyamyd5@gmail.com>
2022-06-17 19:41:52 +0430

    fix login activity for lunch

--- app/src/main/java/com/karimi/pms/activity/Login.kt
+++ app/src/main/java/com/karimi/pms/activity/Login.kt
@@ -2,11 +2,12 @@ package com.karimi.pms.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import com.karimi.pms.R
 
 class LoginEmail : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login_email)
+        setContentView(R.layout.activity_main)
     }
 }
