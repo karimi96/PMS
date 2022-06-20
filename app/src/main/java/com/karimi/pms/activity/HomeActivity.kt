f58e491e28364b1e63708a3099af3134f7360cc1
karimi <zmanyamyd5@gmail.com>
2022-06-20 15:07:36 +0430

    fix item recyclerview

--- app/src/main/java/com/karimi/pms/activity/HomeActivity.kt
+++ app/src/main/java/com/karimi/pms/activity/HomeActivity.kt
@@ -3,10 +3,45 @@ package com.karimi.pms.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.karimi.pms.R
+import com.karimi.pms.adapter.RequestAdapter
+import com.karimi.pms.modal.Request
+import kotlinx.android.synthetic.main.activity_home.*
+import kotlinx.android.synthetic.main.toolbar.*
 
 class HomeActivity : AppCompatActivity() {
+    var listRequest : ArrayList<Request> = ArrayList()
+    private lateinit var adapter :RequestAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        initRecycler()
+
     }
+
+    private fun initRecycler(){
+        initListAdapter()
+        adapter = RequestAdapter(listRequest,applicationContext)
+        recycler.adapter = adapter
+    }
+
+    private fun initListAdapter(){
+        listRequest.add(Request("رفع خرابی","15 خرداد کوچه 39 15 خرداد کوچه 39 15 خرداد کوچه 9","1401/03/28","فوری"))
+        listRequest.add(Request("رفع خرابی","15 خرداد کوچه 39","1401/03/28","فوری"))
+        listRequest.add(Request("نصب","15 خرداد کوچه 39","1401/03/28","امروز"))
+        listRequest.add(Request("نصب","15 خرداد کوچه 39","1401/03/28","امروز"))
+        listRequest.add(Request("نصب","15 خرداد کوچه 39","1401/03/28","امروز"))
+        listRequest.add(Request("نصب","15 خرداد کوچه 39","1401/03/28","امروز"))
+        listRequest.add(Request("نصب","15 خرداد کوچه 39","1401/03/28","امروز"))
+        listRequest.add(Request("نصب","15 خرداد کوچه 39","1401/03/28","امروز"))
+        listRequest.add(Request("بازدید دوره","15 خرداد کوچه 39","1401/03/28","فردا"))
+        listRequest.add(Request("بازدید دوره","15 خرداد کوچه 39","1401/03/28","فردا"))
+        listRequest.add(Request("بازدید دوره","15 خرداد کوچه 39","1401/03/28","فردا"))
+        listRequest.add(Request("بازدید دوره","15 خرداد کوچه 39","1401/03/28","فردا"))
+    }
+
+    fun initSitting(){
+        setting_slider.setOnClickListener {  }
+    }
+
 }
