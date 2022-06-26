8931ad2afe1259f13345be4ff74ea6eec541112c
karimi <zmanyamyd5@gmail.com>
2022-06-26 21:36:26 +0430

    fix show_More in dialog

--- app/src/main/java/com/karimi/pms/activity/HomeActivity.kt
+++ app/src/main/java/com/karimi/pms/activity/HomeActivity.kt
@@ -11,6 +11,7 @@ import com.karimi.pms.adapter.RequestAdapter
 import com.karimi.pms.modal.Request
 import kotlinx.android.synthetic.main.activity_home.*
 import kotlinx.android.synthetic.main.bottom_sheet_filter.*
+import kotlinx.android.synthetic.main.box_chat.*
 import kotlinx.android.synthetic.main.dialog_filter_detail.*
 import kotlinx.android.synthetic.main.toolbar.*
 
@@ -52,7 +53,6 @@ class HomeActivity : AppCompatActivity() ,RequestAdapter.Listener{
         setting_slider.setOnClickListener {
             val dialog = BottomSheetDialog(this)
             dialog.setContentView(R.layout.bottom_sheet_setting)
-            dialog.setCancelable(false)
 //            dialog.window?.setBackgroundDrawableResource(R.drawable.item_border_bottom_sheet)
             dialog.show()
         }
@@ -63,7 +63,6 @@ class HomeActivity : AppCompatActivity() ,RequestAdapter.Listener{
         filtering.setOnClickListener {
             val dialog = BottomSheetDialog(this)
             dialog.setContentView(R.layout.bottom_sheet_filter)
-            dialog.setCancelable(false)
 //            dialog.window?.setBackgroundDrawableResource(R.drawable.item_border_bottom_sheet)
             dialog.show()
         }
@@ -81,6 +80,20 @@ class HomeActivity : AppCompatActivity() ,RequestAdapter.Listener{
                 ((6.3 * width) / 7).toInt(),
                 ViewGroup.LayoutParams.WRAP_CONTENT)
         dialog.close_detail.setOnClickListener { dialog.dismiss() }
+
+        var k = 0
+        dialog.showMore.setOnClickListener {
+            k++
+            if (k%2 != 0 ){
+                dialog.passage_short.visibility = View.GONE
+                dialog.passage_long.visibility = View.VISIBLE
+            }else {
+                dialog.passage_short.visibility = View.VISIBLE
+                dialog.passage_long.visibility = View.GONE
+            }
+       }
+
+
         dialog.tv_doing.setOnClickListener {
             dialog.linear_done_cancel.visibility = View.VISIBLE
             dialog.tv_doing.visibility = View.GONE
