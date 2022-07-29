b36abb3f74a101e4b83cfc4b67c7f157b285b91c
karimi <zmanyamyd5@gmail.com>
2022-07-29 21:47:55 +0430

    add App and Session class

---
+++ app/src/main/java/com/karimi/pms/helper/App.java
@@ -0,0 +1,14 @@
+package com.karimi.pms.helper;
+
+import android.app.Application;
+import android.content.Context;
+
+public class App extends Application {
+    public static Context context;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        context = getBaseContext();
+    }
+}
