74ef5e35f74d1369a5fdf67e3a12e8de5a7b9f3f
karimi <zmanyamyd5@gmail.com>
2022-07-30 19:44:10 +0430

    create Operation class

---
+++ app/src/main/java/com/karimi/pms/modal/operation/Operation.kt
@@ -0,0 +1,15 @@
+package com.karimi.pms.modal.operation
+
+class Operation {
+    var company_id: Int? = null
+    var business_id: Int? = null
+    var user_id: Int? = null
+    var name: String? = null
+    var type: String? = null
+    var status: String? = null
+    var location_force: String? = null
+    var start_fast_time: String? = null
+    var start_on_time: String? = null
+    var start_low_time: String? = null
+
+}
